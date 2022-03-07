2a653d0ce0904e3ef2429db737c7c8ce6191d6c9
Kinuthia Ndung'u <kin.ndungu@gmail.com>
2022-03-07 18:17:28 +0300

    Make video player responsive

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,23 +1,23 @@
 import { Box, Button } from 'grommet';
 
+import './Home.css';
+
 export const Home = () => {
   return (
-    <Box align="center" direction="column" justify="center" margin="large">
-      <iframe
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        frameBorder="0"
-        height="315"
-        src="https://www.youtube.com/embed/fCn8zs912OE"
-        title="YouTube video player"
-        width="560"
-      />
-      <Button
-        alignSelf="center"
-        href="/portuguese"
-        label="Learn Portuguese"
-        margin="large"
-      />
+    <Box align="center" margin="large">
+      <div className="videoContainer">
+        <div className="videoWrapper">
+          <iframe
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            frameBorder="0"
+            src="https://www.youtube.com/embed/fCn8zs912OE"
+            title="YouTube video player"
+          />
+        </div>
+      </div>
+
+      <Button href="/portuguese" label="Learn Portuguese" margin="large" />
     </Box>
   );
 };
